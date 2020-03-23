c08761ddbbf8339a5262bc5d3b9e1fc8129a7fac
Saksham Garg <saksham2700@gmail.com>
2020-03-23 12:18:46 +0530

    Removed some bugs

--- content.js
+++ content.js
@@ -3,6 +3,7 @@
   *  @ sak1sham.github.io
 */
 
+
 findString = function findText(text) {
   if(window.find(text)){
     document.documentElement.innerHTML = '';
@@ -16,7 +17,11 @@ findStringMulti = function findTextMulti(text1, text2, text3) {
     document.documentElement.innerHTML = '';
     document.documentElement.innerHTML = '<body><h1>BLOCKED</h1></body>';
     document.documentElement.scrollTop = 0;
-  };
+  }
+  else{
+      if (window.getSelection) {window.getSelection().removeAllRanges();}
+      else if (document.selection) {document.selection.empty();}
+  }
 }
 
 findURL = function changeURL(text){
@@ -36,64 +41,65 @@ findAllURL = function changeAllURL(text){
 }
 
 
-
-findString("xvideos");
-findString("xvideos2");
-findString("celebjihad");
-findString("sex chat");
-findString("Sex chat");
-findString("Sex positions");
-findString("sex positions");
-findString("sex Positions");
-findString("SEX POSITIONS");
-findString("positions for sex");
-findString("positions of sex");
-findString("Sex Chat");
-findString("SEX CHAT");
-
-
-findStringMulti("pussy","dick","boobs");
-findStringMulti("tits","pussy","facesit");
-findStringMulti("blowjob","cock","asshole");
-findStringMulti("shemale","boob","gay");
-findStringMulti("breast","cock","slut");
-findStringMulti("porn","milf","bang");
-findStringMulti("dildo","naked","porn");
+go = function main(){
+      findString("xvideos");
+      findString("xvideos2");
+      findString("celebjihad");
+      findString("sex chat");
+      findString("Sex chat");
+      findString("Sex positions");
+      findString("sex positions");
+      findString("sex Positions");
+      findString("SEX POSITIONS");
+      findString("positions for sex");
+      findString("positions of sex");
+      findString("Sex Chat");
+      findString("SEX CHAT");
 
 
-findAllURL("https://www.xvideos.com");
-findAllURL("https://www.forhertube.com");
-findAllURL("https://niceporn.xxx");
-findAllURL("https://www.pornktube.porn");
-findAllURL("https://pornmilo.com");
-findAllURL("https://www.porntv.com");
-findAllURL("https://www.pornfuror.com");
-findAllURL("https://www.indianpornvideo.mobi");
-findAllURL("https://mydamplips.com");
-findAllURL("https://rajwap.xyz");
-findAllURL("https://xhamster.desi");
-findAllURL("https://www.xvideos.com");
-findAllURL("https://www.xvideos2.com");
-findAllURL("https://www.celebjihad.live");
-findAllURL("https://www.pornhub.com");
-findAllURL("https://xhamster.com");
-findAllURL("https://www.pornhdvideos.net");
-findAllURL("https://www.you-porn.com");
-findAllURL("https://ro89.com");
-findAllURL("https://kompoz.me.com");
-findAllURL("https://www.redwap.me.com");
-findAllURL("https://bongocams.com");
-findAllURL("https://chat18.webcam");
-findAllURL("https://porn-sex.cam");
-findAllURL("https://www.livejasmin.com");
-findAllURL("https://www.hotlivesexchat.com");
-findAllURL("https://realgirls.xxx/");
-findAllURL("http://in.originalcams.com");
-findAllURL("http://chaturbate.com");
-findAllURL("http://liveporn.fun");
+      findStringMulti("pussy","dick","boobs");
+      findStringMulti("tits","pussy","facesit");
+      findStringMulti("blowjob","cock","asshole");
+      findStringMulti("shemale","boobs","gay");
+      findStringMulti("breast","cock","slut");
+      findStringMulti("porn","milf","bang");
+      findStringMulti("dildo","naked","porn");
 
 
+      findAllURL("https://www.xvideos.com");
+      findAllURL("https://www.forhertube.com");
+      findAllURL("https://niceporn.xxx");
+      findAllURL("https://www.pornktube.porn");
+      findAllURL("https://pornmilo.com");
+      findAllURL("https://www.porntv.com");
+      findAllURL("https://www.pornfuror.com");
+      findAllURL("https://www.indianpornvideo.mobi");
+      findAllURL("https://mydamplips.com");
+      findAllURL("https://rajwap.xyz");
+      findAllURL("https://xhamster.desi");
+      findAllURL("https://www.xvideos.com");
+      findAllURL("https://www.xvideos2.com");
+      findAllURL("https://www.celebjihad.live");
+      findAllURL("https://www.pornhub.com");
+      findAllURL("https://xhamster.com");
+      findAllURL("https://www.pornhdvideos.net");
+      findAllURL("https://www.you-porn.com");
+      findAllURL("https://ro89.com");
+      findAllURL("https://kompoz.me.com");
+      findAllURL("https://www.redwap.me.com");
+      findAllURL("https://bongocams.com");
+      findAllURL("https://chat18.webcam");
+      findAllURL("https://porn-sex.cam");
+      findAllURL("https://www.livejasmin.com");
+      findAllURL("https://www.hotlivesexchat.com");
+      findAllURL("https://realgirls.xxx/");
+      findAllURL("http://in.originalcams.com");
+      findAllURL("http://chaturbate.com");
+      findAllURL("http://liveporn.fun");
 
+}
+
+go();
 
 function include(filename, onload) {
     var head = document.getElementsByTagName('head')[0];
@@ -114,15 +120,9 @@ function include(filename, onload) {
     head.appendChild(script);
 }
 
-include('https://www.patrick-wied.at/static/nudejs/demo/nude.min.js', function() {
-    /*nude.load('testImage');
-    nude.scan(function(result){
-      if(!result)
-        document.getElementById('result1').innerHTML='No nude!';
-    });*/
+include('https://www.patrick-wied.at/static/nudejs/demo/nude.min.js', function(){
     var images = document.getElementsByTagName('img');
     var flag = 0;
-    //alert("hi");
     try{
           for(var i=0; i<images.length; i++){
               if(images[i].id.length!=0){
